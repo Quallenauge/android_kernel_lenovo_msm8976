d6ced803dcb82159aa2d47a0917a193deaccb079
Angelo G. Del Regno <kholk11@gmail.com>
2016-07-02 18:01:51 +0200

    video: msm: mdss_dsi_host: Add DSI PHY revision hack for MSM8994
    Something makes the MDSS DSI to erroneously reply 0x2 (rev20)
    after reading the register for DSI PHY revision.
    This happens (most probably) only on MSM8994.
    
    MSM8936 is tested and unaffected.

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -307,6 +307,10 @@ void mdss_dsi_read_phy_revision(struct mdss_dsi_ctrl_pdata *ctrl)
 		reg_val = reg_val >> 4;
 	}
 
+#ifdef CONFIG_ARCH_MSM8994
+	reg_val = DSI_PHY_REV_10;
+#endif
+
 	if (reg_val == DSI_PHY_REV_20)
 		ctrl->shared_data->phy_rev = DSI_PHY_REV_20;
 	else if (reg_val == DSI_PHY_REV_10)
