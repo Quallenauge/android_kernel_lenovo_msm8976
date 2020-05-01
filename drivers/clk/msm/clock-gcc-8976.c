MotorolaMobilityLLC/kernel-msm

clk: qcom: gcc: Add support for efuse based GFX clock
Add support to read the efuse register and determine the version and based
on the version load the corresponding frequency-voltage mapping table.
Also update the fmax & list of supported frequencies based on the version.

Change-Id: Iea360feba7f5cdc8993c5dc80a2b5c8f59a0f2fb
Signed-off-by: Taniya Das <tdas@codeaurora.org>

drivers/clk/qcom/clock-gcc-8976.c
@@ -1308,6 +1308,28 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src[] = {
 	F_END
 };
 
+static struct clk_freq_tbl ftbl_gfx3d_clk_src_v1[] = {
+	F(  19200000,             xo,    1,    0,     0),
+	F(  50000000,          gpll0,   16,    0,     0),
+	F(  80000000,          gpll0,   10,    0,     0),
+	F( 100000000,          gpll0,    8,    0,     0),
+	F( 133333333,          gpll0,    6,    0,     0),
+	F( 160000000,          gpll0,    5,    0,     0),
+	F( 200000000,          gpll0,    4,    0,     0),
+	F( 228571429,          gpll0,  3.5,    0,     0),
+	F( 240000000,    gpll6_gfx3d,  4.5,    0,     0),
+	F( 266666667,          gpll0,    3,    0,     0),
+	F( 300000000,    gpll4_gfx3d,    4,    0,     0),
+	F( 366670000,          gpll3,    3,    0,     0),
+	F( 400000000,          gpll0,    2,    0,     0),
+	F( 432000000,    gpll6_gfx3d,  2.5,    0,     0),
+	F( 480000000,    gpll4_gfx3d,  2.5,    0,     0),
+	F( 550000000,          gpll3,    2,    0,     0),
+	F( 600000000,    gpll4_gfx3d,    2,    0,     0),
+	F( 621330000,    gpll2_gfx3d,  1.5,    0,     0),
+	F_END
+};
+
 static struct rcg_clk gfx3d_clk_src = {
 	.cmd_rcgr_reg = GFX3D_CMD_RCGR,
 	.set_rate = set_rate_hid,
@@ -3827,6 +3849,36 @@ static struct clk_lookup msm_clocks_gcc_gfx[] = {
 	CLK_LIST(gcc_gtcu_ahb_clk),
 };
 
+static void get_gfx_version(struct platform_device *pdev, int *version)
+{
+	struct resource *res;
+	void __iomem *base;
+	u32 efuse;
+
+	*version = 0;
+
+	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "fuse");
+	if (!res) {
+		dev_err(&pdev->dev,
+			 "No version available. Defaulting to 0.\n");
+		return;
+	}
+
+	base = devm_ioremap(&pdev->dev, res->start, resource_size(res));
+	if (!base) {
+		dev_warn(&pdev->dev,
+			 "Unable to read fuse data. Defaulting to 0.\n");
+		return;
+	}
+
+	efuse = readl_relaxed(base);
+	devm_iounmap(&pdev->dev, base);
+
+	*version = (efuse >> 20) & 0x1;
+
+	dev_info(&pdev->dev, "GFX-Version: %d\n", *version);
+}
+
 static int of_get_fmax_vdd_class(struct platform_device *pdev, struct clk *c,
 								char *prop_name)
 {
@@ -3954,7 +4006,8 @@ static void populate_gpu_opp_table(struct platform_device *pdev)
 static int msm_gcc_gfx_probe(struct platform_device *pdev)
 {
 	struct resource *res;
-	int ret;
+	char prop_name[] = "qcom,gfxfreq-corner-vX";
+	int ret, version;
 	u32 regval;
 
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "cc_base");
@@ -3977,11 +4030,20 @@ static int msm_gcc_gfx_probe(struct platform_device *pdev)
 		return PTR_ERR(vdd_gfx.regulator[0]);
 	}
 
-	ret = of_get_fmax_vdd_class(pdev, &gfx3d_clk_src.c,
-					"qcom,gfxfreq-corner");
+	get_gfx_version(pdev, &version);
+
+	snprintf(prop_name, ARRAY_SIZE(prop_name), "qcom,gfxfreq-corner-v%d",
+					version);
+
+	ret = of_get_fmax_vdd_class(pdev, &gfx3d_clk_src.c, prop_name);
 	if (ret) {
 		dev_err(&pdev->dev, "Unable to get gfx freq-corner mapping info\n");
 		return ret;
+	}
+
+	if (version) {
+		gfx3d_clk_src.freq_tbl = ftbl_gfx3d_clk_src_v1;
+		gcc_oxili_gfx3d_clk.c.fmax[VDD_DIG_HIGH] = 621330000;
 	}
 
 	ret = of_msm_clock_register(pdev->dev.of_node, msm_clocks_gcc_gfx,

Quallenauge/android_kernel_lenovo_msm8976

drivers/clk/msm/clock-gcc-8976.c
@@ -1309,6 +1309,28 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src[] = {
 	F_END
 };
 
+static struct clk_freq_tbl ftbl_gfx3d_clk_src_v1[] = {
+	F(  19200000,             xo,    1,    0,     0),
+	F(  50000000,          gpll0,   16,    0,     0),
+	F(  80000000,          gpll0,   10,    0,     0),
+	F( 100000000,          gpll0,    8,    0,     0),
+	F( 133333333,          gpll0,    6,    0,     0),
+	F( 160000000,          gpll0,    5,    0,     0),
+	F( 200000000,          gpll0,    4,    0,     0),
+	F( 228571429,          gpll0,  3.5,    0,     0),
+	F( 240000000,    gpll6_gfx3d,  4.5,    0,     0),
+	F( 266666667,          gpll0,    3,    0,     0),
+	F( 300000000,    gpll4_gfx3d,    4,    0,     0),
+	F( 366670000,          gpll3,    3,    0,     0),
+	F( 400000000,          gpll0,    2,    0,     0),
+	F( 432000000,    gpll6_gfx3d,  2.5,    0,     0),
+	F( 480000000,    gpll4_gfx3d,  2.5,    0,     0),
+	F( 550000000,          gpll3,    2,    0,     0),
+	F( 600000000,    gpll4_gfx3d,    2,    0,     0),
+	F( 621330000,    gpll2_gfx3d,  1.5,    0,     0),
+	F_END
+};
+
 static struct rcg_clk gfx3d_clk_src = {
 	.cmd_rcgr_reg = GFX3D_CMD_RCGR,
 	.set_rate = set_rate_hid,
@@ -3837,6 +3859,36 @@ static struct clk_lookup msm_clocks_gcc_gfx[] = {
 	CLK_LIST(gcc_gtcu_ahb_clk),
 };
 
+static void get_gfx_version(struct platform_device *pdev, int *version)
+{
+	struct resource *res;
+	void __iomem *base;
+	u32 efuse;
+
+	*version = 0;
+
+	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "efuse");
+	if (!res) {
+		dev_err(&pdev->dev,
+			 "No version available. Defaulting to 0.\n");
+		return;
+	}
+
+	base = devm_ioremap(&pdev->dev, res->start, resource_size(res));
+	if (!base) {
+		dev_warn(&pdev->dev,
+			 "Unable to read fuse data. Defaulting to 0.\n");
+		return;
+	}
+
+	efuse = readl_relaxed(base);
+	devm_iounmap(&pdev->dev, base);
+
+	*version = (efuse >> 20) & 0x1;
+
+	dev_info(&pdev->dev, "GFX-Version: %d\n", *version);
+}
+
 static int of_get_fmax_vdd_class(struct platform_device *pdev, struct clk *c,
 								char *prop_name)
 {
@@ -3964,7 +4016,8 @@ static void populate_gpu_opp_table(struct platform_device *pdev)
 static int msm_gcc_gfx_probe(struct platform_device *pdev)
 {
 	struct resource *res;
-	int ret;
+	char prop_name[] = "qcom,gfxfreq-corner-vX";
+	int ret, version;
 	u32 regval;
 
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "cc_base");
@@ -3987,11 +4040,19 @@ static int msm_gcc_gfx_probe(struct platform_device *pdev)
 		return PTR_ERR(vdd_gfx.regulator[0]);
 	}
 
-	ret = of_get_fmax_vdd_class(pdev, &gfx3d_clk_src.c,
-					"qcom,gfxfreq-corner");
+	get_gfx_version(pdev, &version);
+	snprintf(prop_name, ARRAY_SIZE(prop_name), "qcom,gfxfreq-corner-v%d",
+					version);
+
+	ret = of_get_fmax_vdd_class(pdev, &gfx3d_clk_src.c, prop_name);
 	if (ret) {
 		dev_err(&pdev->dev, "Unable to get gfx freq-corner mapping info\n");
 		return ret;
+	}
+
+	if (version) {
+		gfx3d_clk_src.freq_tbl = ftbl_gfx3d_clk_src_v1;
+		gcc_oxili_gfx3d_clk.c.fmax[VDD_DIG_HIGH] = 621330000;
 	}
 
 	ret = of_msm_clock_register(pdev->dev.of_node, msm_clocks_gcc_gfx,